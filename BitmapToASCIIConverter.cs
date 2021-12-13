f99aa9f5fddbb07f43f231b00f9267675567c634
Danil Ushakov <danila.ushakov.08@mail.ru>
2021-12-13 23:26:07 +0800

    final version

---
+++ BitmapToASCIIConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ASCIIGraphic
+{
+    public class BitmapToASCIIConverter
+    {
+        private readonly char[] ASCIItable = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+        private readonly Bitmap _bitmap;
+        public BitmapToASCIIConverter(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            var result = new char[_bitmap.Height][];
+
+            for(int y = 0; y < _bitmap.Height; y++)
+            {
+                result[y] = new char[_bitmap.Width];
+                for(int x = 0; x < _bitmap.Width; x++)
+                {
+                    int MapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, ASCIItable.Length - 1);
+                    result[y][x] = ASCIItable[MapIndex];
+                }
+            }    
+
+            return result;
+        }
+
+        private float Map(float valueToMap, float start1, float end1, float start2, float end2)
+        {
+            return ((valueToMap - start1) / (end1 - start1) * (end2 - start2)) + start2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ASCIIGraphic
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 1.5;
+        private const int MAX_WIDTH = 200;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.WriteLine("Press enter to start...\n");
+
+            while(true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayScale();
+
+                var Converter = new BitmapToASCIIConverter(bitmap);
+                var rows = Converter.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap map)
+        {
+            var newHeight = map.Height / WIDTH_OFFSET * MAX_WIDTH / map.Width;
+            if(map.Width > MAX_WIDTH || map.Height > newHeight)
+            {
+                map = new Bitmap(map, new Size(MAX_WIDTH, (int)newHeight));
+            }
+            return map;
+        }
+    }
+}
